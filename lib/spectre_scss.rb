22c574f0de100c7963b899a7785b9c1778de33f2
Zoran Pesic <zoran1991@gmail.com>
2017-08-31 18:25:41 -0700

    set up rails engine

--- lib/spectre_scss.rb
+++ lib/spectre_scss.rb
@@ -1,5 +1,5 @@
 require 'spectre_scss/version'
+require 'spectre_scss/engine' if (defined?(Rails) && defined?(Rails::Engine))
 
 module SpectreScss
-
 end
---
+++ lib/spectre_scss/engine.rb
@@ -0,0 +1,4 @@
+module SpectreScss
+  class Engine < Rails::Engine
+  end
+end
